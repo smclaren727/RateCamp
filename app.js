56c99e14866bd2f4c03d56d0e1a1ac05e9ab5e2b
smclaren727 <smclaren727@gmail.com>
2016-02-14 21:09:40 -0500

    Added basic structure for routes. Created campground page with basic listing

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var express = require("express");
 var app = express();
 
-app.listen(process.env.PORT, process.env.IP, function(){
-  console.log("The RateCamp Server Has Started");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+  var campgrounds = [
+    {name: "Salmon Creek", image: "https://www.flickr.com/photos/musubk/14527995244/"},
+    {name: "Granite Hill", image: "https://www.flickr.com/photos/amalakar/7299820870/"},
+    {name: "Mountain Goat's Rest", image: "https://www.flickr.com/photos/alanenglish/3844623716/"}
+  ];
+
+  res.render("campgrounds",{campgrounds:campgrounds});
+});
+
+app.listen(3000, function(){
+  console.log("The RateCamp Server Has Started on Port 3000");
 });
