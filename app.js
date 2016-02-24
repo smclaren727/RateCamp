82a9e92bc24fb3c1d7fe049e1507b85acb3c95b6
smclaren727 <smclaren727@gmail.com>
2016-02-23 22:22:13 -0500

    Created models directory. Refactored Campgrounds schema to a model file.

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    Campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost/rate_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-// SCHEMA SETUP - Refactor soon
-var campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
 //   {
