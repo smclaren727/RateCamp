d18bae5ae2378d95fa01dcf6f78436ed9e0528e3
smclaren727 <smclaren727@gmail.com>
2016-02-24 23:44:56 -0500

    Created seed file and data for campgrounds

---
+++ seeds.js
@@ -0,0 +1,55 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var data = [
+  {name: "Cloud's Rest",
+   image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+   description: "some words go here."
+  },
+  {name: "Desert Mesa",
+   image: "https://farm4.staticflickr.com/3327/4593576497_835bbd10cd.jpg",
+   description: "some words go here."
+  },
+  {name: "Canyon Floor",
+   image: "https://farm8.staticflickr.com/7068/6780970858_9b0e519daf.jpg",
+   description: "some words go here."
+  }
+];
+
+function seedDB(){
+  //Remove all campgrounds
+  Campground.remove({}, function(err){
+    if(err){
+      console.log(err);
+    }
+    console.log("removed campgrounds");
+
+    //Add campgrounds
+    data.forEach(function(seed){
+      Campground.create(seed, function(err, campground){
+        if(err){
+          console.log(err);
+        } else {
+          console.log("added a campground");
+          //Create a comment
+          Comment.create(
+            {
+              text: "This place is great. No wifi!",
+              author: "Traveler"
+            }, function(err, comment){
+              if(err){
+                console.log(err);
+              } else {
+                  campground.comments.push(comment);
+                  campground.save();
+                console.log("Created new comment");
+              }
+            });
+        }
+      });
+    });
+  });
+}
+
+module.exports = seedDB;
