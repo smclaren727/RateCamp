e68813fce24f5e3090a06fe1ef93bbb75ac2f42d
smclaren727 <smclaren727@gmail.com>
2016-02-26 21:05:01 -0500

    Refactored views folders. Created comments route and form.

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ app.get("/", function(req, res){
   res.render("landing");
 });
 
+//Index - show all campgrounds
 app.get("/campgrounds", function(req, res){
   Campground.find({}, function(err, AllCampgrounds){
     if(err){
       console.log(err);
     } else {
-      res.render("index",{campgrounds:AllCampgrounds});
+      res.render("campgrounds/index",{campgrounds:AllCampgrounds});
     }
   });
 });
@@ -40,7 +41,7 @@ app.post("/campgrounds", function(req, res){
 });
 
 app.get("/campgrounds/new", function(req, res){
-  res.render("new.ejs");
+  res.render("campgrounds/new");
 });
 
 //Shows campground info
@@ -50,10 +51,26 @@ app.get("/campgrounds/:id", function(req, res){
       console.log(err);
     } else {
       console.log(foundCampground);
-      res.render("show", {campground: foundCampground});
+      res.render("campgrounds/show", {campground: foundCampground});
     }
   });
 });
+
+// ===================
+// COMMENTS ROUTES
+// ===================
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+  //find campground by id
+  Campground.findById(req.params.id, function(err, campground){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("comments/new", {campground: campground});
+    }
+  });
+});
+
 
 app.listen(3000, function(){
   console.log("The RateCamp Server Has Started on Port 3000");
