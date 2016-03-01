11e8453e7d135d29b67c0daa23d604cd1ad03d0b
smclaren727 <smclaren727@gmail.com>
2016-03-01 16:26:20 -0500

    Added login page and form.

--- app.js
+++ app.js
@@ -132,6 +132,18 @@ app.post("/register", function(req, res){
   });
 });
 
+//show login form
+app.get("/login", function(req, res){
+  res.render("login");
+});
+//login logic
+app.post("/login", passport.authenticate("local",
+  {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }), function(req, res){
+});
+
 app.listen(3000, function(){
   console.log("The RateCamp Server Has Started on Port 3000");
 });
